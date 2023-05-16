4f96ce8b40bf057fdbef1d468054cc943967bdd6
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-05-16 12:28:39 +0530

    refactor: add NetworkModule (di module)

---
+++ app/src/main/java/com/example/justweather/di/NetworkModule.kt
@@ -0,0 +1,23 @@
+package com.example.justweather.di
+
+import com.example.justweather.data.remote.WeatherClient
+import com.example.justweather.data.remote.WeatherClientConstants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun provideWeatherClient(): WeatherClient = Retrofit.Builder()
+        .baseUrl(WeatherClientConstants.BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+        .create(WeatherClient::class.java)
+
+}
