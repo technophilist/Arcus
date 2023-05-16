7b76efbb59c18269704cd78970937efa0ac01fa1
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-05-16 12:42:12 +0530

    test: add WeatherClientTest

---
+++ app/src/test/java/com/example/justweather/data/remote/WeatherClientTest.kt
@@ -0,0 +1,28 @@
+package com.example.justweather.data.remote
+
+import com.example.justweather.BuildConfig
+import com.example.justweather.di.NetworkModule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class WeatherClientTest {
+    private val client = NetworkModule.provideWeatherClient()
+
+    @Test
+    fun `Given valid coordinates, the weather should be successfully fetched`() = runTest {
+        val response = client.getWeatherForCoordinates(
+            apiKey = BuildConfig.OPEN_WEATHER_MAP_API_KEY,
+            latitude = "37.333333",
+            longitude = "-122.028033",
+            units = WeatherClientConstants.Units.CELSIUS
+        ).body()
+        advanceUntilIdle()
+        assert(response != null)
+        assert(response!!.cityName == "Cupertino")
+    }
+
+}
