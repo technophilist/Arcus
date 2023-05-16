09c97d934fdc7dc052130f3b80a36915e4ca9f94
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-05-16 12:15:22 +0530

    refactor: add WeatherClientConstants object

---
+++ app/src/main/java/com/example/justweather/data/remote/WeatherClientConstants.kt
@@ -0,0 +1,26 @@
+package com.example.justweather.data.remote
+
+/**
+ * This object contains constants used by the [WeatherClient].
+ */
+object WeatherClientConstants {
+    /**
+     * The base URL of the weather API.
+     */
+    const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+
+    /**
+     * The endpoints of the weather API.
+     */
+    object EndPoints {
+        const val GET_WEATHER_ENDPOINT = "weather"
+    }
+
+    /**
+     * Used to configure the units returned by the weather API.
+     */
+    object Units {
+        const val CELSIUS = "metric"
+        const val FAHRENHEIT = "imperial"
+    }
+}
