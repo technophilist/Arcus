7c32faf793c9fc8528956732083aad0b65be3bd3
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-05-16 12:51:41 +0530

    docs: add detailed docs for WeatherResponse data class

--- app/src/main/java/com/example/justweather/data/remote/models/WeatherResponse.kt
+++ app/src/main/java/com/example/justweather/data/remote/models/WeatherResponse.kt
@@ -4,8 +4,20 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 /**
- * todo stopship docs - @JsonClass makes use of compile code gen instead of depending on reflextion +
- * generates KOTLIN CODE USING KOTLIN POET
+ * A data class that models the response that would be received when a request to get the
+ * weather of a location is made.
+ *
+ * Note: All response classes are annotated with @JsonClass in order to make Moshi use code-gen.
+ * Making Moshi use code-gen is useful for mainly two reasons:
+ * 1) Moshi, by default uses reflection. This creates overhead during runtime. Using code-gen
+ * makes moshi take advantage of kapt during compile time. This removes the overhead that is
+ * associated with reflection.
+ *
+ * 2) Using code-gen makes moshi use Kotlin Poet under the hood. This makes moshi generate
+ * actual Kotlin classes instead of the Java equivalents of Kotlin classes. This ensures that
+ * Moshi fully understands Kotlin constructs. The best example of this, is nullability.
+ * When Moshi doesn't use codegen, then non-null properties in Kotlin can be assigned null values!
+ * This is because, Moshi generates the Java equivalent of the Kotlin data class. 
  */
 @JsonClass(generateAdapter = true)
 data class WeatherResponse(
