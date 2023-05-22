51c75830b4451f33c60f895583dfa5c05e6c7639
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-05-22 13:19:16 +0530

    refactor: bump compose-bom version to 2023.05.01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
     testImplementation("junit:junit:4.12")
 
     // compose
-    val composeBom = platform("androidx.compose:compose-bom:2023.03.00")
+    val composeBom = platform("androidx.compose:compose-bom:2023.05.01")
     implementation(composeBom)
     androidTestImplementation(composeBom)
 
