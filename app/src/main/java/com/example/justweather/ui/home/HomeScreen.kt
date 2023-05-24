99f98fb57e31b6ab90ea9c0b9023430ac56cafb8
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-05-24 13:36:01 +0530

    docs: add docs explaining the need for the use of LocalConfiguration

--- app/src/main/java/com/example/justweather/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/justweather/ui/home/HomeScreen.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.example.justweather.R
+import androidx.compose.foundation.lazy.LazyListScope
 import com.example.justweather.domain.models.BriefWeatherDetails
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -62,6 +62,16 @@ fun HomeScreen(
     }
 }
 
+/**
+ * A composable that contains a centered [SearchBar] meant to be used in the lazy column defined in
+ * [HomeScreen].
+ *
+ * Note: In this composable the [SearchBar]'s max height and width are constrained to the max height
+ * and width of the screen. Using it in a lazy column using [LazyListScope.item], will cause the app
+ * to crash. This is because the width of the [SearchBar], when expanded is set to infinity. A composable
+ * of width infinity, in a lazy column, will make the app crash. Hence, the size is explicitly
+ * constrained. This might be a bug, and might be fixed in the future.
+ */
 @ExperimentalMaterial3Api
 @Composable
 private fun Header(
@@ -71,7 +81,7 @@ private fun Header(
     onSearchQueryChange: (String) -> Unit,
     onSearchBarActiveChange: (Boolean) -> Unit,
     onSearch: (String) -> Unit,
-    searchBarSuggestionsContent: @Composable() (ColumnScope.() -> Unit)
+    searchBarSuggestionsContent: @Composable (ColumnScope.() -> Unit)
 ) {
     val screenHeight = LocalConfiguration.current.screenHeightDp.dp
     val screenWidth = LocalConfiguration.current.screenWidthDp.dp
@@ -80,8 +90,8 @@ private fun Header(
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
                 .sizeIn(
-                    maxWidth = screenWidth,
-                    maxHeight = screenHeight // todo explain
+                    maxWidth = screenWidth, // see docs for explanation
+                    maxHeight = screenHeight // see docs for explanation
                 ),
             query = currentSearchQuery,
             onQueryChange = onSearchQueryChange,
