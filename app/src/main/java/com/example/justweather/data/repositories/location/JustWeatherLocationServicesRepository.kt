e3587526ca4398bcb747362c4c52dfde3fdc81c3
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-05-29 16:57:40 +0530

    fix: rethrow cancellation exceptions on error in location services repository This commit ensures that JustWeatherLocationServicesRepository#fetchSuggestedPlacesForQuery rethrows the exceptions of type kotlinx.coroutines.CancellationException

--- app/src/main/java/com/example/justweather/data/repositories/location/JustWeatherLocationServicesRepository.kt
+++ app/src/main/java/com/example/justweather/data/repositories/location/JustWeatherLocationServicesRepository.kt
@@ -5,6 +5,7 @@ import com.example.justweather.data.remote.location.LocationClient
 import com.example.justweather.data.remote.location.models.coordinates
 import com.example.justweather.data.remote.location.models.toLocationAutofillSuggestionList
 import com.example.justweather.domain.models.LocationAutofillSuggestion
+import kotlinx.coroutines.CancellationException
 import java.util.UUID
 import javax.inject.Inject
 
@@ -15,9 +16,13 @@ class JustWeatherLocationServicesRepository @Inject constructor(
     private val locationClient: LocationClient
 ) : LocationServicesRepository {
 
-    override suspend fun fetchSuggestedPlacesForQuery(query: String): Result<List<LocationAutofillSuggestion>> =
-        runCatching {
+    override suspend fun fetchSuggestedPlacesForQuery(query: String): Result<List<LocationAutofillSuggestion>> {
+        return if (query.isBlank()) Result.success(emptyList())
+        else try {
             val sessionToken = UUID.randomUUID().toString()
+            // non-null assertions to body() calls are fine here because, if the body is empty,
+            // it essentially means that there was some error during the fetch operation. The
+            // null pointer exception will get caught by the enclosing try catch block.
             val suggestionCoordinatePairs = locationClient.getPlacesSuggestionsForQuery(
                 query = query,
                 accessToken = BuildConfig.MAP_BOX_API_KEY,
@@ -29,12 +34,18 @@ class JustWeatherLocationServicesRepository @Inject constructor(
                     sessionToken = sessionToken
                 ).body()!!.coordinates
             }.toList()
-            suggestionCoordinatePairs.map { (suggestion, coordinates) ->
-                val autoFillSuggestionCoordinate = LocationAutofillSuggestion.Coordinates(
-                    latitude = coordinates.latitude,
-                    longitude = coordinates.longitude
-                )
-                suggestion.toLocationAutofillSuggestionList(autoFillSuggestionCoordinate)
-            }
+            val autofillSuggestionList =
+                suggestionCoordinatePairs.map { (suggestion, coordinates) ->
+                    val autoFillSuggestionCoordinate = LocationAutofillSuggestion.Coordinates(
+                        latitude = coordinates.latitude,
+                        longitude = coordinates.longitude
+                    )
+                    suggestion.toLocationAutofillSuggestionList(autoFillSuggestionCoordinate)
+                }
+            Result.success(autofillSuggestionList)
+        } catch (exception: Exception) {
+            if (exception is CancellationException) throw exception
+            Result.failure(exception)
         }
+    }
 }
