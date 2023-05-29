d0014e8f6a5e39c03435563b6f1da176afc59e4c
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-05-29 11:45:16 +0530

    refactor: add LocationClient#getCoordinatesForPlace

--- app/src/main/java/com/example/justweather/data/remote/location/LocationClient.kt
+++ app/src/main/java/com/example/justweather/data/remote/location/LocationClient.kt
@@ -3,8 +3,10 @@ package com.example.justweather.data.remote.location
 import retrofit2.http.GET
 import retrofit2.http.Query
 import androidx.annotation.IntRange
+import com.example.justweather.data.remote.location.models.CoordinatesResponse
 import com.example.justweather.data.remote.location.models.SuggestionsResponse
 import retrofit2.Response
+import retrofit2.http.Path
 
 /**
  * An interface representing a network based client that provides location services such as
@@ -15,6 +17,10 @@ interface LocationClient {
     /**
      * This method returns a list of place suggestions for a given [query] and returns a
      * [Response] object containing a [SuggestionsResponse] object, representing the suggestions.
+     *
+     * Note: For billing purposes, if this method is called before a consecutive call to
+     * [getCoordinatesForPlace], which is often the case, then a common [sessionToken] must be passed
+     * to both methods for billing purposes.
      *
      * @param accessToken The access token for the API.
      * @param sessionToken The session token.
@@ -27,5 +33,20 @@ interface LocationClient {
         @Query("session_token") sessionToken: String,
         @Query("limit") @IntRange(1, 10) limit: Int = 10
     ): Response<SuggestionsResponse>
+
+    /**
+     * This function gets coordinates for a specific place with [placeId], using the provided
+     * [accessToken] and [sessionToken].
+     *
+     * Note: For billing purposes, if this method is called immediately after [getPlacesSuggestionsForQuery],
+     * which is often the case, then a common [sessionToken] must be passed to both methods
+     * for billing purposes.
+     */
+    @GET(LocationClientConstants.EndPoints.GET_COORDINATES_FOR_PLACE_ID)
+    suspend fun getCoordinatesForPlace(
+        @Path("id") placeId: String,
+        @Query("access_token") accessToken: String,
+        @Query("session_token") sessionToken: String,
+    ): Response<CoordinatesResponse>
 
 }
