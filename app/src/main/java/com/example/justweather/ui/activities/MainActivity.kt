21c3bd16da4089919121e840079eb904368b67c2
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-05-30 14:09:56 +0530

    refactor: use JustWeatherNavigation composable in MainActivity

--- app/src/main/java/com/example/justweather/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/justweather/ui/activities/MainActivity.kt
@@ -4,14 +4,9 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.justweather.BuildConfig
+import com.example.justweather.ui.navigation.JustWeatherNavigation
 import com.example.justweather.ui.theme.JustWeatherTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,7 +14,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, BuildConfig.OPEN_WEATHER_MAP_API_KEY, Toast.LENGTH_SHORT).show()
-        setContent {}
+        setContent {
+            JustWeatherTheme {
+                Surface(content = { JustWeatherNavigation() })
+            }
+        }
     }
 }
