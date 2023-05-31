5d343a92ec886b1c9da5c9d4dd3023e2e666944c
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-05-31 18:54:27 +0530

    refactor: add WeatherResponse#toWeatherDetails mapper

--- app/src/main/java/com/example/justweather/data/remote/weather/models/WeatherResponse.kt
+++ app/src/main/java/com/example/justweather/data/remote/weather/models/WeatherResponse.kt
@@ -1,5 +1,7 @@
 package com.example.justweather.data.remote.weather.models
 
+import com.example.justweather.R
+import com.example.justweather.domain.models.WeatherDetails
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
@@ -17,7 +19,7 @@ import com.squareup.moshi.JsonClass
  * actual Kotlin classes instead of the Java equivalents of Kotlin classes. This ensures that
  * Moshi fully understands Kotlin constructs. The best example of this, is nullability.
  * When Moshi doesn't use codegen, then non-null properties in Kotlin can be assigned null values!
- * This is because, Moshi generates the Java equivalent of the Kotlin data class. 
+ * This is because, Moshi generates the Java equivalent of the Kotlin data class.
  */
 @JsonClass(generateAdapter = true)
 data class WeatherResponse(
@@ -51,3 +53,42 @@ data class WeatherResponse(
         val speed: String
     )
 }
+
+/**
+ * Maps an instance of [WeatherResponse] to an instance of [WeatherDetails].
+ */
+fun WeatherResponse.toWeatherDetails(): WeatherDetails = WeatherDetails(
+    nameOfLocation = cityName,
+    temperature = WeatherDetails.Temperature(
+        currentTemp = additionalInfo.temperature,
+        minTemperature = additionalInfo.currentMinTempAroundLocation,
+        maxTemperature = additionalInfo.currentMaxTempAroundLocation
+    ),
+    wind = WeatherDetails.Wind(speed = wind.speed, direction = wind.directionInDegrees),
+    weatherCondition = WeatherDetails.WeatherCondition(
+        oneWordDescription = weather[0].description,
+        currentWeatherConditionIcon = getWeatherIconResForId(weather[0].weatherIconId)
+    ),
+    humidity = additionalInfo.humidity,
+    pressure = additionalInfo.pressure
+)
+
+/**
+ * Returns an appropriate resource ID for the corresponding [weatherIconId]
+ */
+private fun getWeatherIconResForId(weatherIconId: String): Int = when (weatherIconId) {
+    "01d" -> R.drawable.ic_day_clear // clear sky day
+    "02d", "03d", "04d" -> R.drawable.ic_day_few_clouds // few clouds day, scattered clouds day, broken clouds day
+    "09d", "10d" -> R.drawable.ic_day_rain // shower rain day, rain day
+    "11d" -> R.drawable.ic_day_thunderstorms // thunderstorm day
+    "13d" -> R.drawable.ic_day_snow // snow day
+    "50d" -> R.drawable.ic_mist // mist day
+    "01n" -> R.drawable.ic_night_clear // clear sky night
+    "02n", "03n", "04n" -> R.drawable.ic_night_few_clouds // few clouds night, scattered clouds night, broken clouds night
+    "09n", "10n" -> R.drawable.ic_night_rain // shower rain night, rain night
+    "11n" -> R.drawable.ic_night_thunderstorms // thunderstorm night
+    "13n" -> R.drawable.ic_night_snow // snow night
+    else -> R.drawable.ic_mist // mist day/night
+}
+
+
