aae3c1fec8ec98aea0a52a91db5ec3dbb679dc66
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-05-31 19:21:12 +0530

    test: add JustWeatherWeatherRepositoryTest

--- app/src/test/java/com/example/justweather/data/repositories/weather/JustWeatherWeatherRepositoryTest.kt
+++ app/src/test/java/com/example/justweather/data/repositories/weather/JustWeatherWeatherRepositoryTest.kt
@@ -1,5 +1,37 @@
 package com.example.justweather.data.repositories.weather
 
-import org.junit.jupiter.api.Assertions.*
+import com.example.justweather.di.NetworkModule
+import com.example.justweather.domain.models.WeatherDetails
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
 
-internal class JustWeatherWeatherRepositoryTest
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class JustWeatherWeatherRepositoryTest {
+
+    private val weatherRepository =
+        JustWeatherWeatherRepository(NetworkModule.provideWeatherClient())
+
+    @Test
+    fun `getWeatherForLocation should successfully fetch weather details for a given valid coordinate`() =
+        runTest {
+            val latitude = "37.422131"
+            val longitude = "-122.084801"
+            val result = weatherRepository.getWeatherForLocation(latitude, longitude)
+            assert(result.isSuccess)
+            assert(result.getOrNull() != null)
+        }
+
+    @Test
+    fun `getWeatherForLocation should return an exception for an invalid coordinate`() = runTest {
+        // This is an invalid coordinate because the latitude and longitude values are outside the
+        // valid range of -90 to 90 and -180 to 180 degrees, respectively.
+        val latitude = "1000.0"
+        val longitude = "-2000.0"
+        val result = weatherRepository.getWeatherForLocation(latitude, longitude)
+        assert(result.isFailure)
+        assert(result.exceptionOrNull() != null)
+    }
+}
+
