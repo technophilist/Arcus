95f1a07a02e25b909d0cf82976b39caf379d4681
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-06-02 13:05:56 +0530

    refactor: add BriefWeatherDetails.EmptyBriefWeatherDetails static constant

--- app/src/main/java/com/example/justweather/domain/models/BriefWeatherDetails.kt
+++ app/src/main/java/com/example/justweather/domain/models/BriefWeatherDetails.kt
@@ -1,6 +1,7 @@
 package com.example.justweather.domain.models
 
 import androidx.annotation.DrawableRes
+import com.example.justweather.R
 
 /**
  * A data class that holds brief weather details of a particular location. It can be seen as data
@@ -16,4 +17,18 @@ data class BriefWeatherDetails(
     val currentTemperature: String,
     val shortDescription: String,
     @DrawableRes val shortDescriptionIcon: Int,
-)
+) {
+    companion object {
+        /**
+         * An instance of [BriefWeatherDetails] with all string properties set to "--".
+         */
+        val EmptyBriefWeatherDetails = BriefWeatherDetails(
+            nameOfLocation = "- -",
+            currentTemperature = "- -",
+            shortDescription = "- -",
+            shortDescriptionIcon = R.drawable.ic_scattered_clouds // todo
+        )
+    }
+}
+
+
