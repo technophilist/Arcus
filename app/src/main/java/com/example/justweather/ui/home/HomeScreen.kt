a701925cbcaa5f234293469b60c6803a979f2a10
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-06-17 12:38:00 +0530

    refactor: remove no-longer required suggestion warning in home screen

--- app/src/main/java/com/example/justweather/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/justweather/ui/home/HomeScreen.kt
@@ -233,20 +233,6 @@ private fun AutoFillSuggestionsList(
             CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
         } else {
             LazyColumn {
-                item {
-                    Text(
-                        modifier = Modifier
-                            .padding(horizontal = 16.dp)
-                            .padding(top = 8.dp)
-                            .alpha(alpha = 0.38f), // applied emphasis according to material 3 design spec
-                        text = "The weather information closest to the selected location is " +
-                                "displayed if the weather information for the selected location is " +
-                                "not available.",
-                        style = MaterialTheme.typography.labelSmall,
-                        fontWeight = FontWeight.Normal,
-                        textAlign = TextAlign.Center
-                    )
-                }
                 autofillSuggestionItems(
                     suggestions = suggestions,
                     onSuggestionClick = onSuggestionClick
