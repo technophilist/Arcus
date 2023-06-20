c5303a40abf3ea5942b96f1ad5305269fb1e7823
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-06-20 22:33:30 +0530

    refactor: add CurrentWeatherDetails#toBriefWeatherDetails mapper extension

--- app/src/main/java/com/example/justweather/domain/models/CurrentWeatherDetails.kt
+++ app/src/main/java/com/example/justweather/domain/models/CurrentWeatherDetails.kt
@@ -1,16 +1,29 @@
 package com.example.justweather.domain.models
 
 import androidx.annotation.DrawableRes
+import com.example.justweather.data.remote.weather.models.CurrentWeatherResponse
 
 /**
  * A data class that holds the current weather details for a specific location.
  */
 data class CurrentWeatherDetails(
-    val nameOfLocation:String,
+    val nameOfLocation: String,
     val temperature: String,
     val isDay: Int,
     @DrawableRes val iconResId: Int,
     @DrawableRes val imageResId: Int,
     val latitude: String,
     val longitude: String,
+)
+
+/**
+ * Used to convert an instance of [CurrentWeatherDetails] to an instance of [toBriefWeatherDetails].
+ */
+fun CurrentWeatherDetails.toBriefWeatherDetails(): BriefWeatherDetails = BriefWeatherDetails(
+    nameOfLocation = nameOfLocation,
+    currentTemperature = temperature,
+    shortDescription = "N/A", //todo
+    shortDescriptionIcon = iconResId,
+    latitude = latitude,
+    longitude = longitude
 )
