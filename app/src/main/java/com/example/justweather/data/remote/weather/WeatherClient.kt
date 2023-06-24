d93798f2f998c70c3709086a352161ad1cfdf3a5
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-06-24 17:52:11 +0530

    refactor: add WeatherClient#getHourlyForecast

--- app/src/main/java/com/example/justweather/data/remote/weather/WeatherClient.kt
+++ app/src/main/java/com/example/justweather/data/remote/weather/WeatherClient.kt
@@ -1,9 +1,11 @@
 package com.example.justweather.data.remote.weather
 
 import com.example.justweather.data.remote.weather.models.CurrentWeatherResponse
+import com.example.justweather.data.remote.weather.models.HourlyWeatherInfoResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.time.ZoneId
 
 /**
  * An interface representing a network based weather client.
@@ -20,4 +22,17 @@ interface WeatherClient {
         @Query("precipitation_unit") precipitationUnit: String = WeatherClientConstants.Units.PrecipitationUnit.INCHES,
         @Query("current_weather") shouldIncludeCurrentWeatherInformation: Boolean = true // must always be set to true
     ): Response<CurrentWeatherResponse>
+
+    @GET(WeatherClientConstants.EndPoints.GET_WEATHER_ENDPOINT)
+    suspend fun getHourlyForecast(
+        @Query("latitude") latitude: String,
+        @Query("longitude") longitude: String,
+        @Query("start_date") startDate: String, // YYYY-MM-DD
+        @Query("end_date") endDate: String, // YYYY-MM-DD
+        @Query("timezone") timezone: String = ZoneId.systemDefault().toString(),
+        @Query("precipitation_unit") precipitationUnit: String = WeatherClientConstants.Units.PrecipitationUnit.INCHES,
+        @Query("timeformat") timeFormat: String = WeatherClientConstants.TimeFormats.UNIX_EPOCH_TIME_IN_SECONDS,
+        @Query("hourly") hourlyForecastsToReturn: String = WeatherClientConstants.HourlyForecastItems.DEFAULT_ITEMS
+    ): Response<HourlyWeatherInfoResponse>
+
 }
