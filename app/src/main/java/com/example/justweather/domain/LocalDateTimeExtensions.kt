a14b25418bc693fe52fb2d6a1d4ee482cf458369
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-07-08 13:37:01 +0530

    refactor: add LocalDateTime.hourStringInTwelveHourFormat extension property

---
+++ app/src/main/java/com/example/justweather/domain/LocalDateTimeExtensions.kt
@@ -0,0 +1,29 @@
+package com.example.justweather.domain
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+
+/**
+ * A string that contains the current hour in the following format - "hh a". It also ensures that
+ * the length of the string will be consistent regardless of whether the current hour is a single
+ * digit or not.
+ *
+ * Examples :
+ *  - "10 AM" -> "10 AM"
+ *  - "01 AM" -> " 1 AM" --> notice how the string gets padded with an empty space at the front]
+ */
+val LocalDateTime.hourStringInTwelveHourFormat: String
+    get() {
+        //h - Hour in am/pm (1-12)
+        //m - Minute in hour
+        //a - Am/pm marker
+        val dateTimeFormatter = DateTimeFormatter.ofPattern("hh a")
+        // Add empty characters to the start of the string if the hour is a single digit number
+        // to ensure that the length of the hour text remains constant, regardless of whether
+        // the hour text is a single digit or not.
+        return format(dateTimeFormatter).let {
+            if (it.startsWith("0")) it.replaceFirst('0', ' ')
+            else it
+        }
+    }
