ac79c43d68bdc239fd1ee95ff5d49b80d236ae0e
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-07-14 16:28:13 +0530

    refactor: add WeatherClient#getAdditionalDailyForecastVariables

--- app/src/main/java/com/example/justweather/data/remote/weather/WeatherClient.kt
+++ app/src/main/java/com/example/justweather/data/remote/weather/WeatherClient.kt
@@ -1,5 +1,6 @@
 package com.example.justweather.data.remote.weather
 
+import com.example.justweather.data.remote.weather.models.AdditionalDailyForecastVariablesResponse
 import com.example.justweather.data.remote.weather.models.CurrentWeatherResponse
 import com.example.justweather.data.remote.weather.models.HourlyWeatherInfoResponse
 import retrofit2.Response
@@ -50,11 +51,37 @@ interface WeatherClient {
     suspend fun getHourlyForecast(
         @Query("latitude") latitude: String,
         @Query("longitude") longitude: String,
-        @Query("start_date") startDate: LocalDate, // YYYY-MM-DD
-        @Query("end_date") endDate: LocalDate, // YYYY-MM-DD
+        @Query("start_date") startDate: LocalDate,
+        @Query("end_date") endDate: LocalDate,
         @Query("timezone") timezone: String = ZoneId.systemDefault().toString(),
         @Query("precipitation_unit") precipitationUnit: WeatherClientConstants.PrecipitationUnit = WeatherClientConstants.PrecipitationUnit.INCHES,
         @Query("timeformat") timeFormat: WeatherClientConstants.TimeFormats = WeatherClientConstants.TimeFormats.UNIX_EPOCH_TIME_IN_SECONDS,
         @Query("hourly") hourlyForecastsToReturn: WeatherClientConstants.HourlyForecastItems = WeatherClientConstants.HourlyForecastItems.ALL
     ): Response<HourlyWeatherInfoResponse>
+
+
+    /**
+     * Used to get additional daily forecast variables for the given coordinates.
+     *
+     * @param latitude The latitude of the location.
+     * @param longitude The longitude of the location.
+     * @param startDate The start date of the forecast.
+     * @param endDate The end date of the forecast.
+     * @param timezone The timezone of the location. Default is system default timezone.
+     * @param timeFormat The time format of the forecast. Default is UNIX epoch time in seconds.
+     * @param dailyForecastsToReturn The daily forecasts to return. Default is all forecasts.
+     *
+     * @return Response<AdditionalDailyForecastVariablesResponse>
+     */
+    @GET(WeatherClientConstants.EndPoints.GET_WEATHER_ENDPOINT)
+    suspend fun getAdditionalDailyForecastVariables(
+        @Query("latitude") latitude: String,
+        @Query("longitude") longitude: String,
+        @Query("start_date") startDate: LocalDate,
+        @Query("end_date") endDate: LocalDate,
+        @Query("timezone") timezone: String = ZoneId.systemDefault().toString(),
+        @Query("timeformat") timeFormat: WeatherClientConstants.TimeFormats = WeatherClientConstants.TimeFormats.UNIX_EPOCH_TIME_IN_SECONDS,
+        @Query("daily") dailyForecastsToReturn: WeatherClientConstants.DailyForecastItems = WeatherClientConstants.DailyForecastItems.ALL
+    ): Response<AdditionalDailyForecastVariablesResponse>
+
 }
