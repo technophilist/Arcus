fb57d5d0d680ff83e44839a864cb48b3038d3f8d
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-07-14 16:24:42 +0530

    refactor: add AdditionalDailyForecastVariablesResponse data class

---
+++ app/src/main/java/com/example/justweather/data/remote/weather/models/AdditionalDailyForecastVariablesResponse.kt
@@ -0,0 +1,38 @@
+package com.example.justweather.data.remote.weather.models
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+/**
+ * A response object that contains [AdditionalForecastedVariables] for a specific location.
+ */
+@JsonClass(generateAdapter = true)
+data class AdditionalDailyForecastVariablesResponse(
+    @Json(name = "daily") val additionalForecastedVariables: AdditionalForecastedVariables
+) {
+    /**
+     * Data class containing additional forecasted variables.
+     *
+     * @property minTemperatureForTheDay The minimum temperature for the day.
+     * @property maxTemperatureForTheDay The maximum temperature for the day.
+     * @property maxApparentTemperature The maximum apparent temperature.
+     * @property minApparentTemperature The minimum apparent temperature.
+     * @property sunrise The sunrise time.
+     * @property sunset The sunset time.
+     * @property maxUvIndex The maximum UV index.
+     * @property dominantWindDirection The dominant wind direction.
+     * @property windSpeed The wind speed.
+     */
+    @JsonClass(generateAdapter = true)
+    data class AdditionalForecastedVariables(
+        @Json(name = "temperature_2m_max") val minTemperatureForTheDay: List<Double>,
+        @Json(name = "temperature_2m_min") val maxTemperatureForTheDay: List<Double>,
+        @Json(name = "apparent_temperature_max") val maxApparentTemperature: List<Double>,
+        @Json(name = "apparent_temperature_min") val minApparentTemperature: List<Double>,
+        @Json(name = "sunrise") val sunrise: List<String>,
+        @Json(name = "sunset") val sunset: List<String>,
+        @Json(name = "uv_index_max") val maxUvIndex: List<Double>,
+        @Json(name = "winddirection_10m_dominant") val dominantWindDirection: List<Int>,
+        @Json(name = "windspeed_10m_max") val windSpeed: List<Double>
+    )
+}
