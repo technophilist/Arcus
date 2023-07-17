f384db985b43910d34d0ad7b1de8dbd14b7ef846
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-07-17 20:32:39 +0530

    refactor: add mark/unmarkWeatherEntityAsDeleted methods to SavedWeatherLocationEntity

--- app/src/main/java/com/example/justweather/data/local/weather/JustWeatherDatabaseDao.kt
+++ app/src/main/java/com/example/justweather/data/local/weather/JustWeatherDatabaseDao.kt
@@ -15,6 +15,12 @@ interface JustWeatherDatabaseDao {
     @Upsert
     suspend fun addSavedWeatherEntity(weatherLocationEntity: SavedWeatherLocationEntity)
 
+    @Query("UPDATE savedweatherlocations SET isDeleted = 1 WHERE nameOfLocation = :nameOfWeatherLocationEntity")
+    suspend fun markWeatherEntityAsDeleted(nameOfWeatherLocationEntity: String)
+
+    @Query("UPDATE savedweatherlocations SET isDeleted = 0 WHERE nameOfLocation = :nameOfWeatherLocationEntity")
+    suspend fun markWeatherEntityAsUnDeleted(nameOfWeatherLocationEntity: String)
+
     @Delete
     suspend fun deleteSavedWeatherEntity(weatherLocationEntity: SavedWeatherLocationEntity)
 }
