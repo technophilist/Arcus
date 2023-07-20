19b55932b16e59323592b06c88a47e2d10286052
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-07-20 12:44:27 +0530

    refactor: add CoroutineDispatchersModule (di module)

---
+++ app/src/main/java/com/example/justweather/di/DispatchersModule.kt
@@ -0,0 +1,35 @@
+package com.example.justweather.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+@Qualifier
+annotation class MainDispatcher
+
+@Qualifier
+annotation class IODispatcher
+
+@Qualifier
+annotation class DefaultDispatcher
+
+
+@Module
+@InstallIn(ActivityRetainedComponent::class)
+object CoroutineDispatchersModule {
+    @Provides
+    @DefaultDispatcher
+    fun provideDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @Provides
+    @IODispatcher
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @MainDispatcher
+    fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
