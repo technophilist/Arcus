053c4323289fdd06033791285949f23d0452eafd
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-07-26 20:32:26 +0530

    refactor: add SavedWeatherLocationEntity#toSavedLocation extension method

--- app/src/main/java/com/example/justweather/data/local/weather/SavedWeatherLocationEntity.kt
+++ app/src/main/java/com/example/justweather/data/local/weather/SavedWeatherLocationEntity.kt
@@ -2,6 +2,8 @@ package com.example.justweather.data.local.weather
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.justweather.domain.models.Coordinates
+import com.example.justweather.domain.models.SavedLocation
 
 /**
  * This class represents a saved weather location entity with the specified [nameOfLocation],
@@ -13,4 +15,12 @@ data class SavedWeatherLocationEntity(
     val latitude: String,
     val longitude: String,
     val isDeleted: Boolean = false
+)
+
+/**
+ * Used to map an instance of [SavedWeatherLocationEntity] to an instance of [SavedLocation].
+ */
+fun SavedWeatherLocationEntity.toSavedLocation() = SavedLocation(
+    nameOfLocation = nameOfLocation,
+    coordinates = Coordinates(latitude = latitude, longitude = longitude)
 )
