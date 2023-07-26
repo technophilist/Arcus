5bcc63c9da45099738d9048aeaaae67cf802d31f
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-07-26 14:08:17 +0530

    refactor!: make WeatherDetailViewModel use WeatherDetailScreenUiState

--- app/src/main/java/com/example/justweather/ui/weatherDetail/WeatherDetailViewModel.kt
+++ app/src/main/java/com/example/justweather/ui/weatherDetail/WeatherDetailViewModel.kt
@@ -4,17 +4,15 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.justweather.data.repositories.weather.WeatherRepository
-import com.example.justweather.domain.models.CurrentWeatherDetails
-import com.example.justweather.domain.models.HourlyForecast
-import com.example.justweather.domain.models.PrecipitationProbability
-import com.example.justweather.domain.models.SingleWeatherDetail
+import com.example.justweather.data.repositories.weather.fetchHourlyForecastsForNext24Hours
+import com.example.justweather.data.repositories.weather.fetchPrecipitationProbabilitiesForNext24hours
 import com.example.justweather.ui.navigation.JustWeatherNavigationDestinations.WeatherDetailScreen
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.LocalTime
 import javax.inject.Inject
 
 @HiltViewModel
@@ -29,116 +27,83 @@ class WeatherDetailViewModel @Inject constructor(
     private val nameOfLocation: String =
         savedStateHandle[WeatherDetailScreen.NAV_ARG_NAME_OF_LOCATION]!!
 
-    private val _weatherDetailsOfChosenLocation = MutableStateFlow<CurrentWeatherDetails?>(null)
-    val weatherDetailsOfChosenLocation =
-        _weatherDetailsOfChosenLocation as StateFlow<CurrentWeatherDetails?>
 
-    val isSavedLocation = weatherRepository.getWeatherStreamForPreviouslySavedLocations()
-        .map { savedWeatherDetails ->
-            savedWeatherDetails.any { it.nameOfLocation == weatherDetailsOfChosenLocation.value?.nameOfLocation }
-        }.stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 250L),
-            initialValue = true
-        )
-
-    private val _precipitationProbabilityList =
-        MutableStateFlow<List<PrecipitationProbability>>(emptyList())
-    val precipitationProbabilityList =
-        _precipitationProbabilityList as StateFlow<List<PrecipitationProbability>>
-
-    private val _hourlyForecastList = MutableStateFlow<List<HourlyForecast>>(emptyList())
-    val hourlyForecastList = _hourlyForecastList as StateFlow<List<HourlyForecast>>
-
-    private val _additionalWeatherDetailsList =
-        MutableStateFlow<List<SingleWeatherDetail>>(emptyList())
-    val additionalWeatherDetailsList =
-        _additionalWeatherDetailsList as StateFlow<List<SingleWeatherDetail>>
-
-    private val _uiState = MutableStateFlow(UiState.IDLE)
-    val uiState = _uiState as StateFlow<UiState>
+    private val _uiState = MutableStateFlow(WeatherDetailScreenUiState())
+    val uiState = _uiState as StateFlow<WeatherDetailScreenUiState>
 
     init {
-        viewModelScope.launch {
-            fetchWeatherInfo()
-            fetchAndAssignPrecipitationForecasts()
-            fetchAndAssignHourlyForecasts()
-            fetchAndAssignSingleWeatherDetailList()
-        }
+        viewModelScope.launch { fetchWeatherDetailsAndUpdateState() }
+        weatherRepository.getWeatherStreamForPreviouslySavedLocations()
+            .map { savedWeatherDetails ->
+                savedWeatherDetails.any { it.nameOfLocation == nameOfLocation }
+            }
+            .distinctUntilChanged()
+            .onEach { isSavedLocation ->
+                _uiState.update { it.copy(isPreviouslySavedLocation = isSavedLocation) }
+            }
+            .launchIn(scope = viewModelScope)
     }
 
-    /**
-     * Fetches weather information from the repository, ensuring that the [uiState] is
-     * correctly updated.
-     */
-    private suspend fun fetchWeatherInfo() {
-        _uiState.value = UiState.LOADING
-        val weatherDetails = weatherRepository.fetchWeatherForLocation(
-            nameOfLocation = nameOfLocation,
-            latitude = latitude,
-            longitude = longitude
-        ).getOrNull()?.also { _weatherDetailsOfChosenLocation.value = it }
-
-        _uiState.value = if (weatherDetails == null) UiState.ERROR
-        else UiState.IDLE
-    }
-
-    private suspend fun fetchAndAssignPrecipitationForecasts() {
-        val hourlyPrecipitationProbabilities =
-            weatherRepository.fetchHourlyPrecipitationProbabilities(
+    private suspend fun fetchWeatherDetailsAndUpdateState(): Unit = coroutineScope {
+        _uiState.update { it.copy(isLoading = true) }
+        val weatherDetailsOfChosenLocation = async {
+            weatherRepository.fetchWeatherForLocation(
+                nameOfLocation = nameOfLocation,
                 latitude = latitude,
-                longitude = longitude,
-                dateRange = LocalDate.now()..LocalDate.now().plusDays(1)
-            ).getOrNull() ?: return
-        val precipitationProbabilitiesForTheNext24hours = hourlyPrecipitationProbabilities.filter {
-            val isSameDay = it.dateTime == LocalDateTime.now()
-            if (isSameDay) it.dateTime.toLocalTime() >= LocalTime.now()
-            else it.dateTime > LocalDateTime.now()
-        }.take(24)
-        _precipitationProbabilityList.value = precipitationProbabilitiesForTheNext24hours
-    }
-
-    private suspend fun fetchAndAssignHourlyForecasts() {
-        val hourlyForecasts = weatherRepository.fetchHourlyForecasts(
-            latitude = latitude,
-            longitude = longitude,
-            dateRange = LocalDate.now()..LocalDate.now().plusDays(1)
-        ).getOrNull() ?: return
-        val hourlyForecastsForTheNext24hours = hourlyForecasts.filter {
-            val isSameDay = it.dateTime == LocalDateTime.now()
-            if (isSameDay) it.dateTime.toLocalTime() >= LocalTime.now()
-            else it.dateTime > LocalDateTime.now()
-        }.take(24)
-        _hourlyForecastList.value = hourlyForecastsForTheNext24hours
-    }
-
-    private suspend fun fetchAndAssignSingleWeatherDetailList() {
-        _additionalWeatherDetailsList.value =
+                longitude = longitude
+            ).getOrThrow()
+        }
+        val precipitationProbabilities =
+            async {
+                weatherRepository.fetchPrecipitationProbabilitiesForNext24hours(
+                    latitude = latitude,
+                    longitude = longitude
+                ).getOrThrow()
+            }
+        val hourlyForecasts = async {
+            weatherRepository.fetchHourlyForecastsForNext24Hours(
+                latitude = latitude,
+                longitude = longitude
+            ).getOrThrow()
+        }
+        val additionalWeatherInfoItems = async {
             weatherRepository.fetchAdditionalWeatherInfoItemsListForCurrentDay(
                 latitude = latitude,
                 longitude = longitude
-            ).getOrNull() ?: return
-    }
+            ).getOrThrow()
+        }
 
-    fun addLocationToSavedLocations() {
-        if (weatherDetailsOfChosenLocation.value == null) return
-        viewModelScope.launch {
-            _uiState.value = UiState.LOADING
-            weatherRepository.saveWeatherLocation(
-                nameOfLocation = weatherDetailsOfChosenLocation.value!!.nameOfLocation,
-                latitude = latitude,
-                longitude = longitude
-            )
-            _uiState.value = UiState.IDLE
+        try {
+            _uiState.update {
+                it.copy(
+                    isLoading = false,
+                    weatherDetailsOfChosenLocation = weatherDetailsOfChosenLocation.await(),
+                    precipitationProbabilities = precipitationProbabilities.await(),
+                    hourlyForecasts = hourlyForecasts.await(),
+                    additionalWeatherInfoItems = additionalWeatherInfoItems.await()
+                )
+            }
+        } catch (exception: Exception) {
+            if (exception is CancellationException) throw exception
+            _uiState.update {
+                it.copy(
+                    isLoading = false,
+                    errorMessage = "Oops! An error occurred while fetching the weather details."
+                )
+            }
         }
     }
 
-    /**
-     * An enum class that contains all possible UI states.
-     */
-    enum class UiState {
-        IDLE,
-        LOADING,
-        ERROR
+    fun addLocationToSavedLocations() {
+        viewModelScope.launch {
+            _uiState.update { it.copy(isLoading = true) }
+            weatherRepository.saveWeatherLocation(
+                nameOfLocation = nameOfLocation,
+                latitude = latitude,
+                longitude = longitude
+            )
+            _uiState.update { it.copy(isLoading = false) }
+        }
     }
+
 }
