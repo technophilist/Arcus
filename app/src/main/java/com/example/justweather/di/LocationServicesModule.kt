ad5528a7c7cba11995f1b13fe9daf67924aadc24
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-07-31 13:47:50 +0530

    refactor: add LocationServicesModule (di module)

---
+++ app/src/main/java/com/example/justweather/di/LocationServicesModule.kt
@@ -0,0 +1,17 @@
+package com.example.justweather.di
+
+import androidx.lifecycle.ViewModel
+import com.example.justweather.domain.location.CurrentLocationProvider
+import com.example.justweather.domain.location.JustWeatherCurrentLocationProvider
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class LocationServicesModule {
+
+    @Binds
+    abstract fun bindCurrentLocationProvider(impl: JustWeatherCurrentLocationProvider): CurrentLocationProvider
+}
