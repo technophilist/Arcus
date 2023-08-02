0c65e5b764b28e77ee06a012d92d9a8efed7f206
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-08-02 20:25:27 +0530

    refactor: inject & use ReverseGeocoder in HomeViewModel

--- app/src/main/java/com/example/justweather/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/justweather/ui/home/HomeViewModel.kt
@@ -1,27 +1,30 @@
 package com.example.justweather.ui.home
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.justweather.data.remote.location.ReverseGeocoder
 import com.example.justweather.data.repositories.location.LocationServicesRepository
 import com.example.justweather.data.repositories.weather.WeatherRepository
+import com.example.justweather.data.repositories.weather.fetchHourlyForecastsForNext24Hours
+import com.example.justweather.domain.location.CurrentLocationProvider
 import com.example.justweather.domain.models.BriefWeatherDetails
+import com.example.justweather.domain.models.Coordinates
 import com.example.justweather.domain.models.CurrentWeatherDetails
+import com.example.justweather.domain.models.HourlyForecast
 import com.example.justweather.domain.models.LocationAutofillSuggestion
 import com.example.justweather.domain.models.SavedLocation
 import com.example.justweather.domain.models.toBriefWeatherDetails
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
+    private val currentLocationProvider: CurrentLocationProvider,
+    private val reverseGeocoder: ReverseGeocoder,
     private val locationServicesRepository: LocationServicesRepository,
     private val weatherRepository: WeatherRepository
 ) : ViewModel() {
@@ -29,6 +32,39 @@ class HomeViewModel @Inject constructor(
     private val currentSearchQuery = MutableStateFlow("")
     private val isLoadingAutofillSuggestions = MutableStateFlow(false)
     private val isLoadingSavedLocations = MutableStateFlow(false)
+    private val coordinatesOfCurrentLocation = MutableStateFlow<Coordinates?>(null)
+
+    val weatherDetailsOfCurrentLocation: StateFlow<BriefWeatherDetails?> =
+        coordinatesOfCurrentLocation.filterNotNull()
+            .map { coordinates ->
+                val nameOfLocation = reverseGeocoder.getLocationNameForCoordinates(
+                    coordinates.latitude.toDouble(),
+                    coordinates.longitude.toDouble()
+                ).getOrNull() ?: return@map null // todo : exception handling
+                weatherRepository.fetchWeatherForLocation(
+                    nameOfLocation = nameOfLocation,
+                    latitude = coordinates.latitude,
+                    longitude = coordinates.longitude
+                ).getOrNull()?.toBriefWeatherDetails() // todo : exception handling
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(300),
+                initialValue = null
+            )
+
+    val hourlyForecastsForCurrentLocation: StateFlow<List<HourlyForecast>?> =
+        coordinatesOfCurrentLocation.filterNotNull()
+            .map { coordinates ->
+                weatherRepository.fetchHourlyForecastsForNext24Hours(
+                    latitude = coordinates.latitude,
+                    longitude = coordinates.longitude
+                ).getOrNull() // todo : exception handling
+            }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(300),
+                initialValue = null
+            )
 
     private val weatherDetailsOfSavedLocationsResults: Flow<Result<List<CurrentWeatherDetails>>> =
         weatherRepository.getSavedLocationsListStream()
@@ -87,6 +123,7 @@ class HomeViewModel @Inject constructor(
         initialValue = HomeScreenUiState(isLoadingSavedLocations = true)
     )
 
+
     // a cache that stores the CurrentWeatherDetails of a specific SavedLocation
     private var currentWeatherDetailsCache = mutableMapOf<SavedLocation, CurrentWeatherDetails>()
     private var recentlyDeletedItem: BriefWeatherDetails? = null
@@ -132,5 +169,14 @@ class HomeViewModel @Inject constructor(
                 ).getOrElse { return Result.failure(it) }
         }
         return Result.success(currentWeatherDetailsCache.values.toList())
+    }
+
+    fun fetchWeatherForCurrentUserLocation() {
+        viewModelScope.launch {
+            val coordinatesResult =
+                currentLocationProvider.getCurrentLocation().getOrNull() ?: return@launch
+            coordinatesOfCurrentLocation.value = coordinatesResult
+        }
+
     }
 }
