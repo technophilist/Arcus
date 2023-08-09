4963cd2a599f4173f3f2fc2b291bb91d4accb40b
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-08-09 12:20:42 +0530

    refactor: add TypingAnimatedText composable

---
+++ app/src/main/java/com/example/arcus/ui/components/TypingAnimatedText.kt
@@ -0,0 +1,79 @@
+package com.example.arcus.ui.components
+
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
+import kotlinx.coroutines.delay
+
+/**
+ * A [Text] composable that displays the [text] with a Typewriter like animation.
+ * A Typewriter animation is, as the name suggests, an effect that looks like a typing animation,
+ * as being typed by a typewriter. The [delayBetweenEachChar] param can be used to specify the amount
+ * of time to wait before displaying each character.
+ */
+@Composable
+fun TypingAnimatedText(
+    text: String,
+    modifier: Modifier = Modifier,
+    delayBetweenEachChar: () -> Long = { 50 },
+    color: Color = Color.Unspecified,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+    onTextLayout: (TextLayoutResult) -> Unit = {},
+    style: TextStyle = LocalTextStyle.current
+) {
+    var currentText by rememberSaveable { mutableStateOf("") }
+    LaunchedEffect(Unit) {
+        for (char in text) {
+            currentText += char
+            delay(delayBetweenEachChar())
+        }
+    }
+    Text(
+        text = currentText,
+        modifier = modifier,
+        color = color,
+        fontSize = fontSize,
+        fontStyle = fontStyle,
+        fontWeight = fontWeight,
+        fontFamily = fontFamily,
+        letterSpacing = letterSpacing,
+        textDecoration = textDecoration,
+        textAlign = textAlign,
+        lineHeight = lineHeight,
+        overflow = overflow,
+        softWrap = softWrap,
+        maxLines = maxLines,
+        minLines = minLines,
+        onTextLayout = onTextLayout,
+        style = style
+    )
+}
