f034d21b23cf8cdca678a1d04f3d1e0bc2a307eb
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-08-11 16:58:16 +0530

    test: Add test for ArcusGenerativeTextRepository

---
+++ app/src/test/java/com/example/arcus/data/repositories/textgenerator/ArcusGenerativeTextRepositoryTest.kt
@@ -0,0 +1,66 @@
+package com.example.arcus.data.repositories.textgenerator
+
+import com.example.arcus.data.remote.languagemodel.TextGeneratorClient
+import com.example.arcus.data.remote.languagemodel.models.GeneratedTextResponse
+import com.example.arcus.data.remote.languagemodel.models.MessageDTO
+import com.example.arcus.domain.models.Coordinates
+import com.example.arcus.domain.models.CurrentWeatherDetails
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoMoreInteractions
+import retrofit2.Response
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class ArcusGenerativeTextRepositoryTest {
+
+    private lateinit var textGeneratorClientMock: TextGeneratorClient
+    private lateinit var generativeTextRepository: ArcusGenerativeTextRepository
+
+    @Before
+    fun setup() {
+        textGeneratorClientMock = mock {
+            val fakeResponses = listOf(
+                GeneratedTextResponse.GeneratedResponse(
+                    message = MessageDTO(role = "", content = "")
+                )
+            )
+            val fakeGeneratedTextResponse = GeneratedTextResponse(
+                id = "",
+                created = 1,
+                generatedResponses = fakeResponses
+            )
+            onBlocking { getModelResponseForConversations(any()) }
+                .doReturn(Response.success(fakeGeneratedTextResponse))
+        }
+        generativeTextRepository = ArcusGenerativeTextRepository(textGeneratorClientMock)
+    }
+
+    @Test
+    fun `Generated text caching test`() = runTest {
+        // given an instance of CurrentWeatherDetails
+        val fakeWeatherDetails = CurrentWeatherDetails(
+            nameOfLocation = "Seattle",
+            temperatureRoundedToInt = 72,
+            weatherCondition = "Sunny",
+            isDay = 1,
+            iconResId = 1,
+            imageResId = 1,
+            coordinates = Coordinates(latitude = "47.6062", longitude = "-122.3321")
+        )
+        // when initially generating text based on the above instance
+        generativeTextRepository.generateTextForWeatherDetails(fakeWeatherDetails)
+        // the repository must reach out to the client and make a network request
+        verify(textGeneratorClientMock).getModelResponseForConversations(any())
+        // when generating text based on the above instance, for the second time
+        generativeTextRepository.generateTextForWeatherDetails(fakeWeatherDetails)
+        // the repository must not reach out to the client and make a request.
+        // instead, it should return the originally generated string from the cache.
+        verifyNoMoreInteractions(textGeneratorClientMock)
+    }
+}
