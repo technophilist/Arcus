714aa7a704cce96156b45f6d4d6bba18f461a4c0
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-08-11 16:56:52 +0530

    refactor: add GenerativeTextRepository interface

---
+++ app/src/main/java/com/example/arcus/data/repositories/textgenerator/GenerativeTextRepository.kt
@@ -0,0 +1,18 @@
+package com.example.arcus.data.repositories.textgenerator
+
+import com.example.arcus.domain.models.CurrentWeatherDetails
+
+/**
+ * A repository that contains methods that can be used to generate text based on the provided
+ * parameters.
+ */
+interface GenerativeTextRepository {
+
+    /**
+     * Generates text based on the provided [weatherDetails]. Once text has been generated for
+     * a particular instance of [CurrentWeatherDetails] with it's associated data, subsequent calls to
+     * this method,with instances of [CurrentWeatherDetails], with the same values in it, would result
+     * in the same text being returned.
+     */
+    suspend fun generateTextForWeatherDetails(weatherDetails: CurrentWeatherDetails): Result<String>
+}
