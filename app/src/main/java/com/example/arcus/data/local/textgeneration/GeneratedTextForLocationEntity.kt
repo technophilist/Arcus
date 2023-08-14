c8a98ddd2ff45f577c89e1e32a58a6dfb0ad587f
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-08-14 16:38:12 +0530

    refactor: remove composite key definition from room database

--- app/src/main/java/com/example/arcus/data/local/textgeneration/GeneratedTextForLocationEntity.kt
+++ app/src/main/java/com/example/arcus/data/local/textgeneration/GeneratedTextForLocationEntity.kt
@@ -1,17 +1,15 @@
 package com.example.arcus.data.local.textgeneration
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 /**
  * An entity that contains the [generatedDescription] for a specific location based on a specific
  * set of weather conditions.
  */
-@Entity(
-    tableName = "GeneratedTextForLocationEntities",
-    primaryKeys = ["nameOfLocation", "temperature", "conciseWeatherDescription", "generatedDescription"]
-)
+@Entity(tableName = "GeneratedTextForLocationEntities")
 data class GeneratedTextForLocationEntity(
-    val nameOfLocation: String,
+    @PrimaryKey val nameOfLocation: String,
     val temperature: Int,
     val conciseWeatherDescription: String,
     val generatedDescription: String,
